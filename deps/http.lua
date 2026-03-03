89c73e135d7ec1a51e5eed7efef0ca4a920a4bd2
nilFinx <nulflox@disroot.org>
2026-03-03 17:32:28 +0900

    Rename statusReason to statusMessage for parity

--- deps/http.lua
+++ deps/http.lua
@@ -158,7 +158,7 @@ function ServerResponse:flushHeaders()
     end
   end
   head.code = statusCode
-  head.reason = self.statusReason
+  head.reason = self.statusMessage
   local h = self.encode(head)
   self.socket:write(h)
 end
